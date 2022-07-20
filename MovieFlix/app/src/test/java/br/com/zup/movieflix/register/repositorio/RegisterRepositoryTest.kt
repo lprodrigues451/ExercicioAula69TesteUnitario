50bd0c3d9ccb0174a24af638976123d8bef2646a
lprodrigues451 <lprodrigues451@gmail.com>
2022-07-20 16:42:07 -0300

    Subindo o exercicio da aula69

---
+++ MovieFlix/app/src/test/java/br/com/zup/movieflix/register/repositorio/RegisterRepositoryTest.kt
@@ -0,0 +1,26 @@
+package br.com.zup.movieflix.register.repositorio
+
+import br.com.zup.movieflix.register.model.RegisterModel
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+
+class RegisterRepositoryTest {
+    @Test
+    fun regiterUser_informingAllData() {
+        val registerRepository = RegisterRepository()
+        // Arrange
+        val user = RegisterModel(
+            "Felipe",
+            "felipe.dearaujo@zup.com.br",
+            "melhorProfessorDoMundo"
+        )
+        // ACT
+        val result = registerRepository.registerUser(user)
+
+        // Assert
+        assertEquals(result.userName, "Felipe")
+        assertEquals(result.email, "felipe.dearaujo@zup.com.br")
+        assertEquals(result.password, "melhorProfessorDoMundo")
+    }
+}
